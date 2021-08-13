4e5792ed903fe8a85c43b152b6737126922475d8
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-13 22:25:12 +0530

    Basics of task creation

---
+++ Basics/CreatingTasks/CreatingTasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CreatingTasks
+{
+    class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while(i --> 0)
+            {
+                Console.Write(c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Making a task and starting it immediately
+            Task.Factory.StartNew(() => Write('.'));
+
+            // Creating a task and starting it later
+            var t = new Task(() => Write('?'));
+            t.Start();
+
+            Write('-');
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basics/CreatingTasks/CreatingTasks/README.md
@@ -0,0 +1,6 @@
+
+# Creating Tasks
+Each tasks will be running on a different thread. 
+
+In this example, we can see that we have two tasks that run on different threads
+and one task that run on main thread.
