15e4a03e0e477683f6bdad99ed98bacda21dd27d
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-15 22:34:25 +0530

    Wait example using Thread.Sleep

---
+++ Fundamentals/WaitingWithinTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitingWithinTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var t = new Task(() => {
+                Console.WriteLine("Saying good night before sleep!");
+                Thread.Sleep(3000); // Releases the thread whle SpintWait blocks the thread
+                Console.WriteLine("Saying Hello after a sleep!");
+            });
+            t.Start();
+
+            Console.ReadKey();
+            cts.Cancel();
+            Console.WriteLine("Main Program end");
+        }
+    }
+}
