9abcf3ebbd103ff9d76ed4f2b710c8e1d76e840b
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-15 22:19:02 +0530

    Composite cancellation token example

---
+++ Fundamentals/CompositeCancellation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompositeCancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
+
+            // Combination of all three type of token
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(planned.Token, preventative.Token, emergency.Token);
+
+            Task.Factory.StartNew(() =>
+            {
+                int i = 0;
+                while(true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t");
+                    Thread.Sleep(1000);
+                }
+            });
+
+            Console.ReadKey();
+            emergency.Cancel();
+
+            Console.WriteLine("Main Program done.");
+            Console.ReadKey();
+        }
+    }
+}
