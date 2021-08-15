f9d1c47af31c086325ee1763c33f4c7b1fe1fed2
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-15 22:40:54 +0530

    Waiting for a signal via token within the task

--- Fundamentals/WaitingWithinTask/Program.cs
+++ Fundamentals/WaitingWithinTask/Program.cs
@@ -11,15 +11,25 @@ namespace WaitingWithinTask
             var cts = new CancellationTokenSource();
             var token = cts.Token;
 
-            var t = new Task(() => {
+            var bombTimer = new Task(() =>
+            {
+                Console.WriteLine("You have 5 seconds to disarm");
+                var cancelled = token.WaitHandle.WaitOne(5000);
+                Console.WriteLine(cancelled ? "Disarmed!" : "Boom!");
+            }, token);
+
+            var t = new Task(() =>
+            {
                 Console.WriteLine("Saying good night before sleep!");
                 Thread.Sleep(3000); // Releases the thread whle SpintWait blocks the thread
                 Console.WriteLine("Saying Hello after a sleep!");
+                bombTimer.Start();
+                Console.ReadKey();
+                cts.Cancel();
             });
             t.Start();
 
             Console.ReadKey();
-            cts.Cancel();
             Console.WriteLine("Main Program end");
         }
     }
