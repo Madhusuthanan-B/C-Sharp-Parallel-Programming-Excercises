ba4c6f0420bcf40f55f02c7e1afb86960cb935d6
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-16 23:20:21 +0530

    Exception handling - aggregate exception

---
+++ Fundamentals/ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = Task.Factory.StartNew(() => {
+                throw new InvalidOperationException("Cant perform this") { Source = "t1"};
+            });
+
+            var t2 = Task.Factory.StartNew(() => {
+                throw new AccessViolationException("Cant do this") { Source = "t2" };
+            });
+
+
+            try
+            {
+                Task.WaitAll(t1, t2);
+            }
+            // Aggregate exception is typically designed for TPL
+            catch (AggregateException ae)
+            {
+
+                foreach (var exception in ae.InnerExceptions)
+                {
+                    Console.WriteLine($"Exception {exception.GetType()} from {exception.Source}");
+                }
+            }
+            
+            Console.ReadKey();
+            Console.WriteLine("Main end");
+        }
+    }
+}
