ab2636d2671b2e90cfd4921a754cf7e3a2fb6372
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-16 23:14:19 +0530

    Waiting for tasks example

---
+++ Fundamentals/WaitingForTasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitingForTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var t1 = new Task(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("Task 1 end");
+            }, token);
+
+            t1.Start();
+
+            var t2 = new Task(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Task 2 end");
+            }, token);
+
+            t2.Start();
+
+            Task.WaitAll(new[] { t1, t2 }, 4000, token);
+
+            Console.WriteLine($"Task t1 {t1.Status}");
+            Console.WriteLine($"Task t2 {t2.Status}");
+
+            Console.WriteLine("Main program end.");
+            Console.ReadKey();
+
+        }
+    }
+}
