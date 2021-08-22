4b85e9aec9e0d5181b3b36391b8a7c77e67f3f7f
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-22 23:02:24 +0530

    Mutex example - Waits to get a lock

---
+++ Synchronization-and-Sharing/Synchronization/Mutex/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MutexExample
+{
+    public class BankAccount
+    {
+        public int Balance { get; private set; }
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            var bankAccount = new BankAccount();
+            var mutex = new Mutex();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        bool haveLock = mutex.WaitOne();
+                        try
+                        {
+                            bankAccount.Deposit(100);
+                        }
+                        finally
+                        {
+
+                           if(haveLock)
+                            {
+                                mutex.ReleaseMutex();
+                            }
+                        }
+                        
+                    }
+                }));
+
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        bool haveLock = mutex.WaitOne();
+                        try
+                        {
+                            bankAccount.Withdraw(100);
+                        }
+                        finally
+                        {
+
+                            if (haveLock)
+                            {
+                                mutex.ReleaseMutex();
+                            }
+                        }
+                    }
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Balance in acount {bankAccount.Balance}");
+            Console.ReadKey();
+        }
+    }
+}
