3b5c24fa0d48a9ec473a215ef88d025f915f9958
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-22 22:52:44 +0530

    Lock recursion example with spin lock

---
+++ Synchronization-and-Sharing/Synchronization/LockRecursionExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace LockRecursionExample
+{
+    class Program
+    {
+        // If we specifiy true, then we can catch lock recursion exception
+        // Learning from this ==> Spin lock does not support lock recursion
+
+        static SpinLock sl = new SpinLock(true);
+
+        static void LockRecursion(int depth)
+        {
+            bool lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+            }
+            catch (LockRecursionException e)
+            {
+
+                Console.WriteLine($"Exception: {e}");
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Console.WriteLine($"Took a lock, depth = {depth}");
+                    LockRecursion(depth - 1);
+                    sl.Exit();
+                } else
+                {
+                    Console.WriteLine($"Failed to take a lock, depth = {depth}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LockRecursion(5);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
