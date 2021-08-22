cd6288cbac34bd03e683d6f4c76c479c8bf6f986
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-22 23:50:57 +0530

    Concurrent dictionary simple example

---
+++ ConcurrentCollection/ConcurrentDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace ConcurrentDictionaryExample
+{
+    class Program
+    {
+        private static ConcurrentDictionary<string, string> capitals = new ConcurrentDictionary<string, string>();
+
+        public static void AddDelhi()
+        {
+            bool success = capitals.TryAdd("India", "New Delhi");
+            var who = Task.CurrentId.HasValue ? ("Task " + Task.CurrentId) : "Main thread";
+            Console.WriteLine($"{who} {(success? "added": "did not add")} the element.");
+        }
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(AddDelhi).Wait();
+            AddDelhi();
+        }
+    }
+}
